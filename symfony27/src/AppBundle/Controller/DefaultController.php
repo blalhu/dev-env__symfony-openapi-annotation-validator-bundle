7e5a8e8c4251f8fd2ccf8b8aa016786359e14d07
Kornel Balatoni <blal.hu@gmail.com>
2024-02-15 22:12:39 +0100

    first integration of the bundle

--- symfony27/src/AppBundle/Controller/DefaultController.php
+++ symfony27/src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use Pelso\OpenAPIValidatorBundle\Service\CheckerService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,6 +14,9 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        /** @var CheckerService $checker */
+        $checker = $this->get('pelso.openapi_validator_bundle.checker');
+        $checker->check();
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
